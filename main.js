fe9e81e906de73840322ffc7233f3719de47f792
katie-wasylko <kwasylko@hotmail.com>
2016-03-04 00:41:59 -0500

    js added

---
+++ main.js
@@ -0,0 +1,6 @@
+var intro = alert('Let’s make a story together!');
+var colour = prompt('A colour (orange, indigo, etc.)');
+var creature = prompt('A creature (dragon, moth, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning');
